1007617c925f5c7fa1645b2c6396c1d8eb459a5c
Artem <35739625+Artem26r@users.noreply.github.com>
2022-12-20 03:59:37 +0300

    v.1.3.2

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -5,21 +5,46 @@ import { uid } from "react-uid";
 export default function Movie(props) {
   const {
     nameRu,
+    nameOriginal,
     year,
+    type,
     posterUrlPreview,
-    ratingKinopoisk = "N/A",
-    ratingImdb = "N/A",
+    ratingKinopoisk,
+    ratingImdb,
     genres,
     countries,
-  } = props;
+  } = getCustomer() || props;
+
+  function getCustomer() {
+    return undefined
+  }
+  console.log(nameRu);
+  const genreTranslation = (type) => {
+    switch (type) {
+      case 'FILM':
+        return 'фильм'
+        break;
+      case 'TV_SERIES':
+        return 'сериал'
+        break;
+      case 'MINI_SERIES':
+        return 'мини сериал'
+        break;
+      case 'TV_SHOW':
+        return 'шоу'
+        break;
+      default:
+        break;
+    }
+  }
 
   return (
     <div className={styles.card}>
-      <img src={posterUrlPreview} alt={nameRu} />
+      <img src={posterUrlPreview} alt={nameRu === null ? nameOriginal : nameRu} />
       <div className={styles.info}>
         <div>
-          <div className={styles.name_movie}>{nameRu}</div>
-          <div>{year} год</div>
+          <div className={styles.name_movie}>{nameRu === null ? nameOriginal : nameRu}</div>
+          <div>{year} год - <span>{genreTranslation(type)}</span></div>
           <div className={styles.country}>
             {countries.map((el) => {
               return <span key={uid(el)}>{el.country}</span>;
